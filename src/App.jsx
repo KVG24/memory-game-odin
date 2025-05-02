6f8971cd28d18fb1b6e92dbdaa2d8dc1abe66fa9
KVG24 <141866132+KVG24@users.noreply.github.com>
2025-05-02 11:55:04 +0300

    Add data and assets files
    Create basic fetching and rendering for test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
-            <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-            </button>
+            <WarframesRenderList />
         </>
     );
 }
 
 export default App;
+
+const WarframesRenderList = () => {
+    const [warframes, setWarframes] = useState([]);
+
+    useEffect(() => {
+        fetch("src/data/warframes.json")
+            .then((response) => response.json())
+            .then((data) => {
+                setWarframes(data);
+            })
+            .catch((error) => {
+                console.error("Error fetching JSON:", error);
+            });
+    }, []);
+
+    return (
+        <div className="warframe-list">
+            {warframes.map((wf) => (
+                <div key={wf.name} className="warframe-card">
+                    <img
+                        src={`src/assets/${wf.url}`}
+                        alt={wf.name}
+                        width={100}
+                    />
+                    <p>{wf.name}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
